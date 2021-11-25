48e8205cddc745cab64397794486221fa29a77a5
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-25 18:27:46 +0100

    refactor: Clean Up CompaniesList

--- src/components/CompaniesList.jsx
+++ src/components/CompaniesList.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import { Row, Col, Card, Collapse } from 'antd';
-import AppointmentPicker from './AppointmentPicker';
 import CompanyCard from './CompanyCard';
 
-const { Panel } = Collapse;
 
-const CompaniesList = ({ timeslots }) => {
-    return (
+const CompaniesList = ({ timeslots }) =>  (
         <div className="companies-wrapper">
             {timeslots?.map(timeslot => (
                <CompanyCard timeslot={timeslot} />
             ))}
         </div>
-    )
-}
+);
 
 export default CompaniesList;
